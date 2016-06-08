fffa22324889fc8fd1eebee8434ddbae926368b8
BoOz <booz@rezo.net>
2016-06-08 21:26:23 +0200

    ne pas supprimer une ) finale si une ( avait été ouverte.

--- entites_fonctions.php
+++ entites_fonctions.php
@@ -199,7 +199,11 @@ function traiter_fragments($matches, $type_entite, $texte, $fragments, $id_artic
 	foreach($matches as $s){
 		
 		// nettoyage des entités choppées avec une ,. ou autre.
-		$s = trim(preg_replace("/(?!\))\W+$/u", "", $s));
+		if(strpos($s, "("))
+			$s = trim(preg_replace("/(?!\))\W+$/u", "", $s));
+		else
+			$s = trim(preg_replace("/\W+$/u", "", $s));
+		
 		$s = trim(preg_replace("/^\W+/u", "", $s));
 			
 		if($s == "")
