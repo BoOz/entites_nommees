4196f7467ef1ad87707052e958f02f482258df05
BoOz <booz@rezo.net>
2016-10-03 21:10:42 +0200

    maj mots courants + oups

--- mots_courants.php
+++ mots_courants.php
@@ -2,21 +2,21 @@
 
 /* Listes de MOTS COURANTS qui matchent dans la regexp qui trouve les personnalités, par exemple parce qu'ils sont en début de phrase avec une majuscule. On va isoler les echapper de la regexp  */
 
-$adverbes = "Encore|Autant|Après|Autre|Cela|Ceci|Sauf|Voici|Voilà|Pour|Parce|Cher|Chère|Comme|De|Selon|Si|Mais|En|Ainsi|Même|Avec|Tout|tous|toutes|Sans|Avant|Quels?|malgré|Chaque|Parmi|Ne|Non|Pas|Toujours|Nul|Tant|Celle-ci|Celui-ci|Peut-être|Grâce|Contrairement|Presque" ;
-$pronoms = "Les?|La|Aux?|Ces?|Cet+e?|Celui|celles?|ceux|Tous|toute?s?|Des|Aucune?|Du" ;
+$adverbes = "Encore|Autant|Après|Autre|Cela|Ceci|Sauf|Voici|Voilà|Pour|Parce|Cher|Chère|Comme|De|Selon|Si|Mais|En|Ainsi|Même|Avec|Tout|tous|toutes|Sans|Avant|Quels?|malgré|Chaque|Parmi|Ne|Non|Pas|Toujours|Nul|Tant|Celle-ci|Celui-ci|Peut-être|Grâce|Contrairement|Presque|Plutôt|Afin|Surtout" ;
+$pronoms = "Les?|La|Aux?|Ces?|Cet+e?|Celui|celles?|ceux|Tous|toute?s?|Des|Aucune?|Du|Ça" ;
 $verbes = "Lire|Voir|Etant|Est(?:-ce)?|se|Peut-on|Reste|A-t-elle|A-t-il" ;
 $conjonctions = "Mais|Ou|Et|Donc|Or|Ni|Car" ;
 $conj = "Je|Tu|Il|elles?|on|Nous|Vous|Ils|Lui" ;
-$compt = "Une?|Deux|Trois|Quatre|Cinq|Six|Sept|Huit|Neuf|Dix|Plusieurs|Premier|Première|Deuxième|Troisième|Certaines|Certains?|Beaucoup|Laquelle|Les?quels?|Quelque" ;
+$compt = "Une?|Deux|Trois|Quatre|Cinq|Six|Sept|Huit|Neuf|Dix|Plusieurs|Premier|Première|Deuxième|Troisième|Certaines|Certains?|Beaucoup|Laquelle|Les?quels?|Quelque|Trop" ;
 $coord = "De|Par|Pour|Sans" ;
 $coord2 = "Pourquoi|Hormis" ;
 $poss = "Notre|Votre|Vos|mon|Ma|ton|son|sa|ses|nos|Leurs?";
 $autres = "Qui|Que|quoi|dont|Où|Quant|Quel|Quelles?" ;
 $loc = "Sous|Sur|dans|Près|Loin|Là|Ici|Devant|Au-delà|Face|Derrière|Contre";
 $quant = "Quelques|Nombre|Très|Peu|plus|moins|Certains";
-$temps = "Quand|Hier|aujourd|Lors|Depuis|avant|après|pendant";
-$jargon = "Cité|Originaire|Né|Née|Mort";
-$singleton = "Internet|Dieu|Eglise|Djihad|Prophète|Toile";
+$temps = "Quand|Hier|aujourd|Lors|Depuis|avant|après|pendant|Longtemps|Début|Fin|Parfois";
+$jargon = "Cité|Originaire|Né|Née|Mort|Côté|Personne|Ancien|Résultats?|Ayant|Soit|Faute|Sorti|Parfois|Fort|Fondé";
+//$singleton = "Internet|Dieu|Eglise|Djihad|Prophète|Toile";
 
 define("MOTS_DEBUT", $adverbes . 
 					"|" . $pronoms . 
