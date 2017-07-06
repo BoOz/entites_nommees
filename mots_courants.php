70da7b40c81c441710cb551f0ad6276507714da3
Booz <booz@Jade.local>
2017-07-06 21:51:46 +0200

    debug

--- mots_courants.php
+++ mots_courants.php
@@ -39,7 +39,7 @@ define("MOTS_DEBUT", $adverbes .
 
 // Mots au milieu de la séquence cherchée en regex
 
-$mots_milieu = "et|dans|à|pour|où|comme|ou|aux|au|and|par|est|ces|selon|sur|avec|contre|ni|une|sans|entre|depuis|jusqu'(?:au|à)|que|devant|sous|d'après|après|rue|\-\-|Dieu|derrière" ;
+$mots_milieu = "et|dans|à|pour|où|comme|ou|aux|au|and|par|est|ces|selon|sur|avec|contre|ni|une|sans|entre|depuis|jusqu'(?:au|à)|que|devant|sous|d'après|après|rue|\-\-|Dieu|derrière|encore" ;
 $verbes_milieux = "rencontre|dirige|intimide|sera|(?:ré)?invente|prévoit|menac|accus" ;
 
 define("MOTS_MILIEU" ,	$mots_milieu . 
@@ -54,7 +54,7 @@ define("ENTITES_PERSO","Machiavel|Molière|Mirabeau|Staline|Lénine|Mao|Bono|Mus
 /* Patterns pour isoler des entités restées parmis des personnalités */
 
 define("ENTITES_LIEUX_HEURISTIQUE","Sud$|^Nord-|Nord$|Est$|Ouest$|Côte|Congo|République|Etats|City");
-define("ENTITES_INSTITUTIONS_HEURISTIQUE","Nation|Ambassade|Conseil|Fédération|Fondation|Foreign|olympique|Culture|Report|Nouvelle|^The|Parti|Patriot|Musée|Parlement|Press$|^Presses|Agence|University|Agreement|Observatory|Company|Fédération|Edition|News|Centre|démocrat|America|Association|Public|Chambre|Air|Watch$|United|diplomati|Comité|Corporation|Center|Administration|convention|accords|Institut");
+define("ENTITES_INSTITUTIONS_HEURISTIQUE","Nation|Ambassade|Conseil|Fédération|Fondation|Foreign|olympique|Culture|Report|Nouvelle|^The|Parti|Patriot|Musée|Parlement|Press$|^Presses|Agence|University|Agreement|Observatory|Company|Fédération|Edition|News|Centre|démocrat|America|Association|Public|Chambre|Air|Watch$|United|diplomati|Comité|Corporation|Center|Administration|convention|accords|Institut|(?:É|E)tat");
 
 // http://typo.mondediplo.net/?page=entites_nommees&entite=fonctions
 define("FONCTIONS_PERSONNALITES_TYPO","présidente? de la République|chef de l'exécutif|chef de l'(?:É|E)tat,premier ministre|président fédéral|chancelière fédérale|chef du gouvernement|reine|chancelier fédéral|première ministre|roi des Belges|roi-dragon|présidence collégiale tournante|président du conseil des ministres|président du présidium de l'Assemblée populaire suprême depuis 2011|président du gouvernement|ministre d'Etat|roi|présidente?|président du conseil|président du gouvernement|vice-président de la fédération|Guide suprême|président de l'État|président du conseil|empereur|émir|président de la Chambre des représentants|prince régnant|grand-duc|premier ministre par intérim|prince|sultan|ministre d'État|PDG|président de l'Autorité palestinienne|gouverneur|président de la Roumanie|président de la fédération|secrétaire d'État pour les affaires extérieures et politiques|président du gouvernement de Serbie|premier ministre et président du Yuan exécutif|souverain pontife|pape|secrétaire d'(?:É|E)tat");
