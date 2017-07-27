67e453bc02fdbeb9ca2f257ab996d3c199f2fd14
BoOz <booz@rezo.net>
2017-07-27 14:34:07 +0200

    synchro typodiplo

--- spip-cli/sync_data.sh
+++ spip-cli/sync_data.sh
@@ -9,24 +9,33 @@ function typoDiploBrut {
 echo "Mise à jour des dictionnaires." ;
 cd plugins/entites_nommees/
 
-echo $PWD
+# Synchro Google Sheet
 
 echo "Alias sur Google spreadsheets"
-
 curl "https://docs.google.com/spreadsheets/d/1ks_VyPlc3dAzGjrV08j1OaP6bKh4umuOjE92jCqfhAo/pub?gid=0&single=true&output=tsv" > recaler-gsp.txt 
-
-# nettoyer les sauts de lignes
-new=$(cat recaler-gsp.txt | tr '\r' '\n' | tr -s '\n')
+new=$(cat recaler-gsp.txt | tr '\r' '\n' | tr -s '\n') # nettoyer les sauts de lignes
 echo "$new" > recaler.txt
 rm recaler-gsp.txt
-
-git diff > diff-recaler.txt
-
+git diff HEAD -- recaler.txt > diff-recaler.txt
 echo "Mise à jour des alias"
 cat diff-recaler.txt
 
+
+# Synchro TypoDiplo
+
 echo "/* Mise à jour typoDiplo */"
+cd listes_lexicales
 
 echo "Mise à jour des partis"
-typoDiploBrut "http://typo.mondediplo.net/?page=entites_nommees&entite=partis_formations_politiques" > listes_lexicales/Partis_politiques/partis_typo.txt
+typoDiploBrut "http://typo.mondediplo.net/?page=entites_nommees&entite=partis_formations_politiques" > Partis_politiques/partis_typo.txt
 
+
+# Diff
+
+cd ../
+git diff  HEAD -- listes_lexicales/ > listes_lexicales/diff.txt
+cat listes_lexicales/diff.txt
+
+echo "\n Valider le diff et mettre à jour la BDD ? [o/n]"
+read valider
+[[ $valider == "o" ]] && echo "C'est parti !\n" || exit
