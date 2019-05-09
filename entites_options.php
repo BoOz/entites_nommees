4a962c5b9d277afa822d17bb99cd036edbffe9ce
BoOz <booz@rezo.net>
2019-05-09 17:23:42 +0200

    Ne pas faire time out en mode spip-cli

--- entites_options.php
+++ entites_options.php
@@ -4,6 +4,9 @@ if (!defined('_SECTEUR_ENTITES')) {
 	define('_SECTEUR_ENTITES',1);
 }
 
-// informer le plugin indexer_diplo
+// informer le plugin indexer_diplo 
 define('_INDEXER_ENTITES_NOMMEES', true);
-if (!defined('_TIME_OUT')) define('_TIME_OUT', time() + 15);
+if (!defined('_TIME_OUT')
+&& !preg_match("/spip/", $_ENV['_']) // sauf SPIP_CLI
+)
+	define('_TIME_OUT', time() + 15);
